1eec2931b065eb406194d736675d785105f14bf7
unknown <splicecub3@gmail.com>
2019-05-17 01:41:21 -0700

    Project start

---
+++ LanceALot/LanceALot/Assets/FloorClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorClass : MonoBehaviour
+{
+
+    Sprite currentFloor;
+    BoxCollider2D bounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounds = gameObject.GetComponentInChildren<BoxCollider2D>();
+        bounds.size.Set(bounds.size.x - 10,bounds.size.y - 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
